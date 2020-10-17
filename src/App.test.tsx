27e6e2ff5d46e735730e546485e99b6bd92615a2
Rich Churcher <rich.churcher@gmail.com>
2020-10-17 20:55:16 +1300

    Difficulty, category, and quantity selectors

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Category selector is rendered", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const selector = getByText(/choose a trivia category/i);
+  expect(selector).toBeInTheDocument();
+});
+
+test("Difficulty selector is rendered", () => {
+  const { getByText } = render(<App />);
+  const selector = getByText(/select difficulty level/i);
+  expect(selector).toBeInTheDocument();
+});
+
+test("Quantity selector is rendered", () => {
+  const { getByText } = render(<App />);
+  const selector = getByText(/how many questions would you like/i);
+  expect(selector).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <label htmlFor="questionCategory">Choose a trivia category</label>
+      <select name="questionCategory" />
+      <label htmlFor="questionQuantity">
+        How many questions would you like?
+      </label>
+      <input type="number" name="questionQuantity" />
+      <label htmlFor="questionDifficulty">Select difficulty level</label>
+      <select name="questionDifficulty" />
+    </>
   );
 }
 
