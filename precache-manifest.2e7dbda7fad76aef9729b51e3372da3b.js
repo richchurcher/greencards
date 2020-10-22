5da7cd9ebda0fb094b3bb657de1bb7b700abd368
richchurcher <richchurcher@users.noreply.github.com>
2020-10-22 20:26:42 +0000

    deploy: 6c3d5ea0e2c57b29f3bcd8b052a153cda78c1f64

---
+++ precache-manifest.2e7dbda7fad76aef9729b51e3372da3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5951d65a736627d4d73d6ee2fa5bd0b9",
+    "url": "/greencards/index.html"
+  },
+  {
+    "revision": "3429fb189833d2a6ffb6",
+    "url": "/greencards/static/css/main.77db83ca.chunk.css"
+  },
+  {
+    "revision": "fcede9214e186558b7e5",
+    "url": "/greencards/static/js/2.9a6402af.chunk.js"
+  },
+  {
+    "revision": "1af2e467dd1dd801ef7041afe25404ab",
+    "url": "/greencards/static/js/2.9a6402af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3429fb189833d2a6ffb6",
+    "url": "/greencards/static/js/main.56ac4075.chunk.js"
+  },
+  {
+    "revision": "cfcacea520b7fb81a83b",
+    "url": "/greencards/static/js/runtime-main.8b5deed7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dff057bbf947ad3e171fca8003dddb73.js"
+  "/greencards/precache-manifest.2e7dbda7fad76aef9729b51e3372da3b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/greencards/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
