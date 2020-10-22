8d7f11abded83ad4c4d921f07c643dd277ad0cfe
richchurcher <richchurcher@users.noreply.github.com>
2020-10-22 20:22:44 +0000

    deploy: 148a33c41f9ce8908c90fcea9caebaec57851c0c

---
+++ precache-manifest.dff057bbf947ad3e171fca8003dddb73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e35b172662ee5dbcd90a060894d83d24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1f579ed5f73056ba481",
+    "url": "/static/css/main.77db83ca.chunk.css"
+  },
+  {
+    "revision": "ca12889500fbfb718fe5",
+    "url": "/static/js/2.87219a52.chunk.js"
+  },
+  {
+    "revision": "1af2e467dd1dd801ef7041afe25404ab",
+    "url": "/static/js/2.87219a52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1f579ed5f73056ba481",
+    "url": "/static/js/main.f5b382ee.chunk.js"
+  },
+  {
+    "revision": "2ab811bef05af5ab0b71",
+    "url": "/static/js/runtime-main.258bae04.js"
+  }
+]);
