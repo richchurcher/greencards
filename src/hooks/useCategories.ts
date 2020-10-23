592aa79b91879482dbeda403d4e84ee2153cab34
Rich Churcher <rich.churcher@gmail.com>
2020-10-23 17:24:10 +1300

    Remove defunct category cache

--- src/hooks/useCategories.ts
+++ src/hooks/useCategories.ts
@@ -3,16 +3,6 @@ import { useEffect, useState } from "react";
 import config from "../config";
 import { Category } from "../store/category";
 
-let categoryCache: Category[];
-
-export const getCategories = () => {
-  if (!categoryCache) {
-    throw Error("Category cache is not populated.");
-  }
-
-  return categoryCache;
-};
-
 export const useCategories = () => {
   const [categories, setCategories] = useState<Category[]>();
   const [error, setError] = useState<Error>();
@@ -24,7 +14,6 @@ export const useCategories = () => {
         setLoading(true);
         const { data } = await axios.get(config.categoryApiUrl);
         setCategories(data.trivia_categories);
-        categoryCache = data.trivia_categories;
       } catch (e) {
         setError(Error(e.message));
       } finally {
